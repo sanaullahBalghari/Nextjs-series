99c5ad88939955cf5d5efd20bd7056741f50cdc1
sanaullahBalghari <sanaullahakhonzada289@gmail.com>
2025-12-29 01:49:25 +0500

    initail commit

---
+++ mystery-messages/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import AuthProvider from '../context/AuthProvider';
+import { Toaster } from '@/components/ui/toaster';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/ui/Footer';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'True Feedback',
+  description: 'Real feedback from real people.',
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en" >
+      <AuthProvider>
+        <body className={inter.className}>
+          <Navbar/>
+          {children}
+          <Footer/>
+          <Toaster />
+        </body>
+      </AuthProvider>
+    </html>
+  );
+}
+
