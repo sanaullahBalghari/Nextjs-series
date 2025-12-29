d75920b37887671f818c03e1ac155b8c75ac868c
sanaullahBalghari <sanaullahakhonzada289.com>
2025-12-29 20:20:30 +0500

    update

--- mystery-messages/src/components/ui/carousel.tsx
+++ mystery-messages/src/components/ui/carousel.tsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 import useEmblaCarousel, {
-  type EmblaCarouselType as CarouselApi,
+  type UseEmblaCarouselType as CarouselApi,
   type EmblaOptionsType as CarouselOptions,
   type EmblaPluginType as CarouselPlugin,
 } from "embla-carousel-react"
+
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
